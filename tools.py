efae8c48b0022f0bd51a0609d18fdb8dec28d0d5
Prokuma <prokuma@prokuma.kr>
2019-08-19 15:28:09 +0900

    getTempなど実装

--- tools.py
+++ tools.py
@@ -1,6 +1,10 @@
+import serial
+import smbus
+
 # 7Segmentに出力
 def sevenSeg(number):
-    pass
+    s = serial.Serial(''. 9600)
+    s.write(str(number))
 
 # 読みあげ
 def TTS(string):
@@ -12,7 +16,13 @@ def getWeight():
 
 # 温度測定
 def getTemp():
-    pass
+    word_data = bus.read_word_data(address_adt7410, register_adt7410)
+    data = (word_data & 0xff00) >> 8 | (world_data & 0xff) << 8
+    data = data >> 3
+    if data & 0x1000 == 0:
+        return data * 0.0625
+    else:
+        return ((~data&0x1fff) + 1) * -0.0625
 
 # 電源の設定(ON, OFF)
 def setPower(isOn):
