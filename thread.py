9db0012a7fc67112f998b1123501f96e6beb7d21
Prokuma <prokuma@prokuma.kr>
2019-08-20 18:39:40 +0900

    説明するだけの処理

--- thread.py
+++ thread.py
@@ -14,7 +14,7 @@ class Task:
             step = self.json['steps'][self.now]
             
             # TTSで読み上げ処理
-            TTS(step['description'])
+            self.tools.TTS(step['description'])
 
             if step['type'] == 'heat':
                 # 加熱処理
@@ -27,14 +27,13 @@ class Task:
                         self.tools.setPower(False)
                     elif step['heat_strength'] - 5 > temp:
                         self.tools.setPower(True)
-                    self.tools.setPower(True)
                     time.sleep(0.1)
                 self.tools.setPower(False)
                 
                 # Nextを押すまでに待機
                 while self.isContinue:
                     time.sleep(0.1)
-            else:
+            elif step['type'] == 'add':
                 # 追加処理
                 weight_zero = self.tools.getWeight()
                 while self.isContinue:
@@ -44,13 +43,16 @@ class Task:
 
                     weight = self.tools.getWeight() - weight_zero
                     if step['add_grams'] - 10 < weight and step['add_grams'] + 10 > weight:
-                        tools.TTS('適量です'.format(step['add_grams'] - weight))
+                        self.tools.TTS('適量です'.format(step['add_grams'] - weight))
                     else:
                         if start % 15 == 0:
                             if step['add_gram'] > weight:
-                                tools.TTS('あと{}グラムを入れてください'.format(step['add_grams'] - weight))
+                                self.tools.TTS('あと{}グラムを入れてください'.format(step['add_grams'] - weight))
                             else:
-                                tools.TTS('あと{}グラムを抜いてください'.format(weight - step['add_grams']))
+                                self.tools.TTS('あと{}グラムを抜いてください'.format(weight - step['add_grams']))
                     time.sleep(0.1)
-            
+            else:
+                while self.isContinue:
+                    time.sleep(0.1)
+
             self.now = self.now + 1
